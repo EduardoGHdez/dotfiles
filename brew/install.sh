3ab6ebc33680076b615bf40e8219cc343758e161
Eduardo Hernandez <eduardoghdez.io@gmail.com>
2021-07-09 11:57:02 -0500

    caskroom/cask was moved. Tap homebrew/cask instead

--- brew/install.sh
+++ brew/install.sh
@@ -4,4 +4,4 @@ echo "Installing homebrew 🏠"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo "Installing cask 🎧"
-brew tap caskroom/cask
+brew tap homebrew/cask
